00238cb5845b90044ee7f67b5624e4fab5ba2243
TopazSkylar <138179868+TopazSkylar@users.noreply.github.com>
2025-02-10 09:58:02 +1300

    Ensures that an answer has to be given

---
+++ C_02_not_blank.py
@@ -0,0 +1,14 @@
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this cannot be blank. Please try again. \n")
+
+    # main routine starts here
+who = not_blank("Please enter your name: ")
+print(f"Hello {who}")
